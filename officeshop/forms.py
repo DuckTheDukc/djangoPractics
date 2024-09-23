3a1cace4cbb3255785b5ee4435459e5f6c955550
DuckTheDukc <166407070+DuckTheDukc@users.noreply.github.com>
2024-09-23 19:53:00 +0400

    Add files via upload

---
+++ officeshop/forms.py
@@ -0,0 +1,63 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import User
+
+
+
+class RegisterForm(UserCreationForm):
+    username = forms.CharField(
+        widget = forms.TextInput(
+        attrs={
+            'autocomplete': 'text',
+            'placeholder': 'Введите логин',
+            }
+        ),
+        required=False,
+        validators=[RegexValidator(r'[^0-9а-яА-ЯёЁ]', "Введите логин латиницей")],
+    )
+    email = forms.EmailField(
+        widget = forms.EmailInput(
+        attrs={
+            'autocomplete': 'email',
+            'placeholder': 'Введите электронную почту',
+            }
+        ),
+        required=False
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Введите пароль',
+            }
+        ),
+        required=False
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Повторите пароль',
+            }
+        ),
+        required=False
+    )
+    def clean_password1(self):
+        password = self.cleaned_data['password1']
+        if password == '':
+            raise forms.ValidationError('Введите пароль', code='invalid')
+        return password
+    
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if username == '':
+            raise forms.ValidationError('Введите логин', code='invalid')
+        return username
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if email == '':
+            raise forms.ValidationError('Введите электронную почту', code='invalid')
+        return email
+    
+    class  Meta(UserCreationForm.Meta): 
+        fields=("username", "email", "password1", "password2")
---
+++ officeshop/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import get_homepage, get_about, get_products, get_product_detail, search_product, register
+
+
+urlpatterns = [
+    path('about/', get_about, name='about'),
+    path('products/', get_products, name='products'),
+    path('', get_homepage, name='home'),
+    path('product/<int:pk>', get_product_detail, name='product_detail'),
+    path('search', search_product, name='search'),
+    path('register', register, name='register')
+]
